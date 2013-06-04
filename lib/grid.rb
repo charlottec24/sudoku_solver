0364139d70298a8c5484f1014285fde11b427b20
Charlotte Cameron <charlottemidgley@gmail.com>
2013-06-04 14:43:19 +0100

    Further changes, row and column blocks working.

--- lib/grid.rb
+++ lib/grid.rb
@@ -3,21 +3,32 @@ class Grid
 	attr_accessor :sudoku_string
 
 	def initialize(input)
-		@sudoku_string = input.split('')
+		@sudoku_string = input.split('').map { |x| x.to_i }
 	end
 
-	def row
-		@row = @sudoku_string.map { |x| x.to_i }.each_slice(9).to_a
+	def rows(cell_index)
+		@row_index = cell_index / 9
+		row_start_index = @row_index * 9
+		row_end_index = row_start_index + 9 - 1
+		@sudoku_string[row_start_index..row_end_index]
+	end
+	# @sudoku_string.each_slice(9).to_a
+
+
+	def columns(cell_index)
+		@column_index = cell_index % 9
+		column_values = []
+		column_values << @sudoku_string[@column_index]
+		8.times do
+			column_values << @sudoku_string[@column_index += 9]
+		end
+		column_values
 	end
 
-	def column
-		@column = 
-	end
-
-	def subgrid
-
+	def subgrids(cell_index)
+		row_block_index = @row_index / 3
+		column_block_index = @column_index / 3
+		subgrid_start_row_index = (3 * row_block_index * 9) + (3 * column_block_index)
+		subgrid_values
 	end
 end
-
-c = Grid.new('123364475657656757657650000')
-puts c.row.inspect
--- tests/test_grid.rb
+++ tests/test_grid.rb
@@ -1,29 +1,29 @@
 require 'minitest/autorun'
 require '../lib/grid.rb'
 
-class TestGrid < Minitest::Unit::TestCase
+class TestGrid < MiniTest::Test
 
 	def setup
-		@string = Grid.new("005360000704000000032000804080500070360801050000020090900100200100009703050730060")
+		@grid = Grid.new("005360000704000000032000804080500070360801050000020090900100200100009703050730060")
 	end
 
 	def test_input
-		assert_equal 81, @string.sudoku_string.size
+		assert_equal 81, @grid.sudoku_string.size
+		assert_equal 06, @grid.sudoku_string[4]
+	end	
+
+  	def test_row
+  		assert_equal [7, 0, 4, 0, 0, 0, 0, 0, 0], @grid.rows(11)  
+  	end
+	
+	def test_column
+		assert_equal [0, 0, 4, 0, 0, 0, 0, 3, 0], @grid.columns(35)
+		assert_equal [0, 0, 8, 0, 0, 0, 2, 7, 0], @grid.columns(24)
 	end
 
-	def test_row
-		assert_equal [0,0,5,3,6,0,0,0,0], @string.row[0]
-    assert_equal [7,0,4,0,0,0,0,0,0], @string.row[1]
+	def test_subgrid
+		assert_equal [0, 0, 5, 7, 0, 4, 0, 3, 2], @grid.subgrids(0)
 	end
-
-  def test_column
-    assert_equal [0,7,0,0,3,0,9,1,0], @string.column[0]
-  end
-
-  def test_subgrid
-    assert_equal [0,0,5,0,8,0,9,0,0], @string.subgrid[0]
-  end
 end
-
 
 
