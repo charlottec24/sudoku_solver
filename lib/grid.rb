1f18818a462f61143291904fe09aaa268e5e9c5a
Tom Wisniewski <tom.a.wisniewski@gmail.com>
2013-06-06 16:05:55 +0100

    Refactored rows, columns, subgrids
    - removed duplication
    - changed how the array indices are calculated

--- lib/grid.rb
+++ lib/grid.rb
@@ -7,32 +7,35 @@ class Grid
 	end
 
 	def rows(cell_index)
-		@sudoku_array[(cell_index / 9) * 9..((cell_index / 9) * 9) + 8]
+		start_index_of_row = (cell_index / 9) * 9
+		last_index_of_row = start_index_of_row + 8 
+		@sudoku_array[start_index_of_row..last_index_of_row]
 	end
 
 	def columns(cell_index)
 		column_index = cell_index % 9
 		column_values = []
-		column_values << @sudoku_array[column_index]
-		8.times do
-			column_values << @sudoku_array[column_index += 9]
+		(0..8).each do |x|
+			column_values << @sudoku_array[column_index + x * 9]
 		end
 		column_values
 	end
 
 	def subgrids(cell_index)
+		subgrid_start_row_index = subgrid_start_row_index_calculate(cell_index)
+		subgrid_values = []
+		(0..2).each do |x|
+			(0..2).each do |y|
+				subgrid_values << @sudoku_array[subgrid_start_row_index + (9 * x) + y]	
+			end
+		end
+		subgrid_values
+	end
+
+	def subgrid_start_row_index_calculate(cell_index)
 		row_block_index = (cell_index / 9) / 3
 		column_block_index = (cell_index % 9) / 3
 		subgrid_start_row_index = (3 * row_block_index * 9) + (3 * column_block_index)
-		subgrid_values = []
-		subgrid_values << @sudoku_array[subgrid_start_row_index]
-		3.times do |x|
-			subgrid_values << @sudoku_array[subgrid_start_row_index + 1]
-			subgrid_values << @sudoku_array[subgrid_start_row_index + 2]
-			break if x == 2
-			subgrid_values << @sudoku_array[subgrid_start_row_index += 9]	
-		end
-		subgrid_values
 	end
 
 	def solved?
